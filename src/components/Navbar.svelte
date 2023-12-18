1a094852f0dfbf2283d8d88195b3e17535a05c5e
Ohlin Arellano <ohlin.arellano@gmail.com>
2023-12-17 20:19:15 -0800

    global css variables and navbar scaffolding

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,1 +1,30 @@
-<p>navbar</p>
+<nav>
+    <div class="brand">
+        <a href="/">
+            <img src="/assets/brand/logo.png" alt="IT Workforce Talent Pipeline Project Logo">
+        </a>
+    </div>
+    <div class="tabs">
+        <a href="https://www.google.com">
+            Showcase
+        </a>
+        <a href="https://www.google.com">
+            Events
+        </a>
+        <a href="https://www.google.com">
+            About
+        </a>
+        <a href="https://www.google.com">
+            Portal
+        </a>
+        <a href="https://www.google.com">
+            Apply
+        </a>
+    </div>
+</nav>
+
+<style lang="scss">
+    * {
+        color: var(--text);
+    }
+</style>
