f149aaaed0f61be0d2578aef51cd1279d69fa086
SaturnKai <55664707+SaturnKai@users.noreply.github.com>
2023-12-17 20:44:03 -0800

    add benefits and introduction

--- src/components/Benefit.svelte
+++ src/components/Benefit.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	export let title: string;
+	export let color: string;
 </script>
 
 <div class="benefit">
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
-		width="24"
-		height="24"
 		viewBox="0 0 24 24"
 		fill="none"
 		stroke="currentColor"
@@ -14,8 +13,40 @@
 		stroke-linecap="round"
 		stroke-linejoin="round"
 		class="icon lucide lucide-graduation-cap"
+		style="stroke: {color};"
 		><path d="M22 10v6M2 10l10-5 10 5-10 5z" /><path d="M6 12v5c3 3 9 3 12 0v-5" /></svg
 	>
 	<h2 class="title">{title}</h2>
 	<p class="description"><slot /></p>
 </div>
+
+<style lang="scss">
+	.benefit {
+		background: #fbfbfb;
+		border: 1px solid #ededed;
+		border-radius: 10px;
+		padding: 1em 1.2em;
+		width: 350px;
+		transition: background 100ms ease;
+
+		&:hover {
+			background: #f4f4f4;
+		}
+	}
+
+	.icon {
+		width: 2.2rem;
+	}
+
+	.title {
+		font-size: 1.1rem;
+		margin-top: 0.3em;
+	}
+
+	.description {
+		font-size: 0.9rem;
+		color: #4e4e4e;
+		margin-top: 1em;
+		line-height: 1.5;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,83 @@
 	import Benefit from 'components/Benefit.svelte';
 </script>
 
-<div class="benefits">
-	<Benefit title="Training and Credits">
-		Technical & employability skills training, resulting in <b>6 UNLV</b> credits and
-		<b>1 high school</b> credit.
-	</Benefit>
+<div class="section">
+	<h2 class="section-title">What do we have to offer?</h2>
+	<p class="description">
+		This <b>year-long</b> program accelerates learning for high school juniors and seniors with
+		<b>IT experience</b>, guiding them towards advanced education and <b>IT careers</b>. Aligned
+		with the national <b>IT Competency Model</b>, it focuses on foundational, technical, and
+		job-specific skills through internships. Our <b>2023-2024</b> cohort features
+		<b>30 Grade 11 & 12 students</b> poised for success.
+	</p>
+	<img class="image" src="/assets/cat-bread.png" alt="cat eating bread placeholder." />
 </div>
+
+<div class="benefits">
+	<div class="benefits-row">
+		<Benefit title="Training and Credits" color="#B01F24">
+			Technical & employability skills training, resulting in <b>6 UNLV</b> credits and
+			<b>1 high school</b> credit.
+		</Benefit>
+		<Benefit title="Mentorship and Tutoring" color="#888CBA">
+			Online assistance with <b>course material</b> and <b>general questions</b>.
+		</Benefit>
+		<Benefit title="Interview and Internships" color="#B01F24">
+			A <b>guaranteed interview</b> for program completers, including a <b>full-time paid</b> internship.
+		</Benefit>
+	</div>
+	<div class="benefits-row">
+		<Benefit title="Paid Certification Fees" color="#888CBA">
+			Technical & employability skills training, resulting in <b>6 UNLV</b> credits and
+			<b>1 high school</b> credit.
+		</Benefit>
+		<Benefit title="Full-Time Summer Internship" color="#B01F24">
+			Online assistance with <b>course material</b> and <b>general questions</b>.
+		</Benefit>
+		<Benefit title="Over 4,000$ in Value" color="#888CBA">
+			A <b>guaranteed interview</b> for program completers, including a <b>full-time paid</b> internship.
+		</Benefit>
+	</div>
+</div>
+
+<style lang="scss">
+	.section {
+		margin-left: 180px;
+		margin-top: 80px;
+	}
+
+	.section-title {
+		margin-bottom: 20px;
+		position: relative;
+
+		&::before {
+			content: '';
+			position: absolute;
+			width: 3px;
+			height: 28px;
+			background: #b01f24;
+			transform: translateX(-20px);
+		}
+	}
+
+	.description {
+		line-height: 1.8;
+		max-width: 500px;
+	}
+
+	.image {
+		width: 200px;
+		border-radius: 8px;
+	}
+
+	.benefits {
+		margin-top: 80px;
+	}
+
+	.benefits-row {
+		display: flex;
+		gap: 1rem;
+		justify-content: center;
+		margin-bottom: 20px;
+	}
+</style>
