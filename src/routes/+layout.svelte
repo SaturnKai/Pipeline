6dd58143ffc71de7e04ec88dba611a30ef2c604a
SaturnKai <55664707+SaturnKai@users.noreply.github.com>
2023-12-17 20:02:15 -0800

    structure layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Navbar from 'components/Navbar.svelte';
+	import Footer from 'components/Footer.svelte';
+</script>
+
+<header>
+	<Navbar />
+</header>
+<main>
+	<slot />
+</main>
+<footer>
+	<Footer />
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,6 @@
 	import Benefit from 'components/Benefit.svelte';
 </script>
 
-<h1>Hello world!</h1>
-
-<style lang="scss">
-	h1 {
-		color: red;
-
-		&:hover {
-			color: blue;
-		}
-	}
-</style>
+<div class="benefits">
+	<Benefit />
+</div>
